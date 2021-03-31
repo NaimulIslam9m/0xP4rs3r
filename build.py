ce74eeb27bd30544dab854be902b3f23ac347a54
Naim <naim2018331019@gmail.com>
2021-03-31 15:52:15 +0600

    spacing

---
+++ build.py
@@ -0,0 +1,20 @@
+from subprocess import getoutput
+
+inp = input()
+if 'false' in inp:
+    inp = inp.replace('false,', 'False,')
+if 'true' in inp:
+    inp = inp.replace('true,', 'True,')
+
+json = eval(inp)
+tests = json['tests']
+problem_no = json['name'][0]
+
+getoutput('rm Testcase/' + problem_no + '*')
+getoutput('code ' + problem_no + '.py')
+
+for i in range(1, len(tests) + 1):
+    with open('Testcase/' + problem_no + str(i) + '.in', 'w') as f:
+        f.write(tests[i - 1]['input'])
+    with open('Testcase/' + problem_no + str(i) + '.out', 'w') as f:
+        f.write(tests[i - 1]['output'])
