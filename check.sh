1ad702e7f6dd9485deda67381afee2ea268590fd
Naim <naim2018331019@gmail.com>
2021-03-31 15:53:25 +0600

    spacing and color added

---
+++ check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+tc=1
+
+BCyan='\033[1;36m'
+BRed='\033[1;31m'
+BGreen='\033[1;32m'
+NC='\033[0m' # No Color
+
+OK="${BCyan}*${NC}"
+ERROR="${BRed}!{NC}"
+
+for x in `ls Testcase/$1*.in`; do
+    cat $x | python3 $1.py > Testcase/$1$tc.res
+    diff -y --suppress-common-lines Testcase/$1$tc.res Testcase/$1$tc.out > Testcase/diff.txt
+
+    if [[ `echo $?` == 1 ]]; then
+        echo -e "${BRed} WA On Test $tc ${NC}"
+        cat Testcase/diff.txt
+    else
+        echo -e "${BGreen} PASSED ${NC}"
+    fi
+    ((tc+=1))
+done
